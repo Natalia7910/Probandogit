8b638194902c7e4b29150162c89dc41be28f3d27
Natalia7910 <antosepure0719@hotmail.com>
2023-06-12 22:50:06 -0500

    avance_3

--- js/index.js
+++ js/index.js
@@ -1,34 +1,7 @@
+const bar = document.querySelector(".fa-bars");
+const menu = document.querySelector(".menu");
 
-<script>
-    function validateForm() {
-    var nameInput = document.getElementById("nameInput");
-    var priceInput = document.getElementById("priceInput");
-    var inventoryInput = document.getElementById("inventoryInput");
+bar.addEventListener("click", () => {
+    menu.classList.toggle("show-menu");
+});
 
-    var nameError = document.getElementById("nameError");
-    var priceError = document.getElementById("priceError");
-    var inventoryError = document.getElementById("inventoryError");
-
-    if (nameInput.value === "") {
-        nameError.innerText = "Campo requerido";
-            } else {
-        nameError.innerText = "";
-            }
-
-    if (priceInput.value === "") {
-        priceError.innerText = "Campo requerido";
-            } else {
-        priceError.innerText = "";
-            }
-
-    if (inventoryInput.value === "") {
-        inventoryError.innerText = "Campo requerido";
-            } else {
-        inventoryError.innerText = "";
-            }
-
-    if (nameInput.value !== "" && priceInput.value !== "" && inventoryInput.value !== "") {
-        alert("¡Producto creado con éxito!");
-            }
-        }
-</script>
